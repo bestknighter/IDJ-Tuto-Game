97108f92bedc8fb79051f59e3b7f1809d4d3eb39
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-03-30 15:01:07 -0300

    Finaliza implementacao de Rect

--- include/Rect.hpp
+++ include/Rect.hpp
@@ -1,6 +1,8 @@
 #ifndef _RECT_HPP_
 #define _RECT_HPP_
 
+#include "Vec2.hpp"
+
 class Rect {
   public:
     float x;
@@ -11,6 +13,12 @@ class Rect {
     Rect (float x, float y, float w, float h);
     Rect operator+ (Rect& other);
     Rect operator- (Rect& other);
+    void Mover (Vec2& mov);
+    void SetTamanho (Vec2& novoTamanho);
+    Vec2 GetTamanho ();
+    Vec2 GetPosicao ();
+    Vec2 GetCentro ();
+    bool isInside (Vec2& point);
 };
 
 #endif //_RECT_HPP_
--- src/Rect.cpp
+++ src/Rect.cpp
@@ -21,11 +21,37 @@ Rect Rect::operator+ (Rect& other) {
 }
 
 
-Rect Rect::operator+ (Rect& other) {
+Rect Rect::operator- (Rect& other) {
     return Rect (
         x - other.x,
         y - other.y,
         w - other.w,
         h - other.h
     );
+}
+
+void Rect::Mover (Vec2& mov) {
+    x += mov.x;
+    y += mov.y;
+}
+
+void Rect::SetTamanho (Vec2& novoTamanho) {
+    w = novoTamanho.x;
+    h = novoTamanho.y;
+}
+
+Vec2 Rect::GetTamanho () {
+    return Vec2 (w, h);
+}
+
+Vec2 Rect::GetPosicao () {
+    return Vec2 (x, y);
+}
+
+Vec2 Rect::GetCentro () {
+    return Vec2 ((x + w)/2, (y + h)/2);
+}
+
+bool Rect::isInside (Vec2& point) {
+    return x <= point.x && point.x <= x+w && y <= point.y && point.y <= y+h;
 }
