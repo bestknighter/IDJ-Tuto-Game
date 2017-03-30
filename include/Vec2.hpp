35613631e25a0e761df254f4ae01aafd42b51bed
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-03-30 15:00:27 -0300

    Finaliza implementacao de Vec2

--- include/Vec2.hpp
+++ include/Vec2.hpp
@@ -1,14 +1,30 @@
 #ifndef _VEC2_HPP_
 #define _VEC2_HPP_
 
+#include <cmath>
+
 class Vec2 {
   public:
     float x;
     float y;
     Vec2 ();
     Vec2 (float x, float y);
+    float GetMagnitude ();
+    float GetRadianos ();
+    static Vec2 FromPolar (float magnitude, float radianos);
     Vec2 operator+ (Vec2& other);
     Vec2 operator- (Vec2& other);
+    Vec2 operator* (float other);
+    Vec2 operator/ (float other);
+    Vec2 GetNormal ();
+    Vec2 Rotate (float radianos);
+    static float Dot (Vec2& lhs, Vec2& rhs);
+    static float AnguloEntre (Vec2& lhs, Vec2& rhs);
+    static Vec2 i;
+    static Vec2 j;
+  private:
+    float magnitude;
+    float radianos;
 };
 
 #endif //_VEC2_HPP_
--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,5 +1,8 @@
 #include "Vec2.hpp"
 
+Vec2 Vec2::i(1, 0);
+Vec2 Vec2::j(0, 1);
+
 Vec2::Vec2 () {
     x = y = 0;
 }
@@ -7,6 +10,20 @@ Vec2::Vec2 () {
 Vec2::Vec2 (float x, float y) {
     this->x = x;
     this->y = y;
+    magnitude = sqrt (pow (x, 2) + pow (y, 2));
+    radianos = atan2 (x, y);
+}
+
+float Vec2::GetMagnitude () {
+    return magnitude;
+}
+
+float Vec2::GetRadianos () {
+    return radianos;
+}
+
+Vec2 Vec2::FromPolar (float magnitude, float radianos) {
+    return Vec2 (magnitude * cos (radianos), magnitude * sin (radianos));
 }
 
 Vec2 Vec2::operator+ (Vec2& other) {
@@ -15,4 +32,31 @@ Vec2 Vec2::operator+ (Vec2& other) {
 
 Vec2 Vec2::operator- (Vec2& other) {
     return Vec2 (x - other.x, y - other.y);
+}
+
+Vec2 Vec2::operator* (float other) {
+    return Vec2 (x * other, y * other);
+}
+
+Vec2 Vec2::operator/ (float other) {
+    return Vec2 (x / other, y / other);
+}
+
+Vec2 Vec2::GetNormal () {
+    return Vec2 (x / magnitude, y / magnitude);
+}
+
+Vec2 Vec2::Rotate (float radianos) {
+    return Vec2 (
+        x * cos (radianos) - y * sin (radianos),
+        y * cos (radianos) + x * sin (radianos)
+    );
+}
+
+float Vec2::Dot (Vec2& lhs, Vec2& rhs) {
+    return lhs.x * rhs.x + lhs.y * rhs.y;
+}
+
+float Vec2::AnguloEntre (Vec2& lhs, Vec2& rhs) {
+    return acos (Dot (lhs, rhs) / (lhs.magnitude * rhs.magnitude));
 }
