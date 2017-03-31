64faffc56b1f22c44c94df7b58f96b093a0b4617
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-03-31 15:33:14 -0300

    Declara mas nao define TileMap

---
+++ include/TileMap.hpp
@@ -0,0 +1,30 @@
+#ifndef _TILEMAP_HPP_
+#define _TILEMAP_HPP_
+
+#include <string>
+#include <vector>
+
+class TileMap;
+
+#include "TileSet.hpp"
+
+class TileMap {
+  public:
+    TileMap (std::string file, TileSet* tileSet);
+    void Load (std::string file);
+    void SetTileSet (TileSet* tileSet);
+    int& At (int x, int y, int z = 0);
+    void Render (int cameraX = 0, int cameraY = 0);
+    void RenderLayer (int layer, int cameraX = 0, int cameraY = 0);
+    int GetWidth ();
+    int GetHeight ();
+    int GetDepth ();
+  private:
+    std::vector<int> tileMatrix;
+    tileSet* tileSet;
+    int mapWidth;
+    int mapHeight;
+    int mapDepth;
+};
+
+#endif //_TILEMAP_HPP_
---
+++ src/TileMap.cpp
@@ -0,0 +1,5 @@
+#include "tileMap.hpp"
+
+TileMap::TileMap (std::string file, TileSet* tileSet) {
+    
+}
