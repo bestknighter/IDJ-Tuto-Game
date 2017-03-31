c761c871ac0cf4c2a1d40e9611f598b16aa8f986
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-03-31 03:11:59 -0300

    Modifica classe State para realizar spawn e de-spawn de pinguins

--- include/State.hpp
+++ include/State.hpp
@@ -2,6 +2,8 @@
 #define _STATE_HPP_
 
 #include <string>
+#include <vector>
+#include <memory>
 
 #ifdef _WIN32
     #include "SDL.h"
@@ -17,17 +19,25 @@
 class State;
 
 #include "Sprite.hpp"
+#include "GameObject.hpp"
 
 class State {
   public:
     State ();
+    ~State ();
     bool QuitRequested ();
     void LoadAssets ();
     void Update (float dt);
     void Render ();
   private:
+    void Input ();
+    void AddObject (float mouseX, float mouseY);
     Sprite bg;
     bool quitRequested = false;
+    std::vector<std::unique_ptr<GameObject>> objectArray;
 };
+
+#include "Vec2.hpp"
+#include "Face.hpp"
 
 #endif // _STATE_HPP_
--- src/State.cpp
+++ src/State.cpp
@@ -4,6 +4,10 @@ State::State () : bg (std::string ("./resources/img/ocean.jpg")) {
     quitRequested = false;
 }
 
+State::~State() {
+    objectArray.clear ();
+}
+
 bool State::QuitRequested () {
     return quitRequested;
 }
@@ -13,9 +17,52 @@ void State::LoadAssets () {
 }
 
 void State::Update (float dt) {
-    quitRequested = SDL_QuitRequested ();
+    Input();
+    for (int i = objectArray.size () - 1; i >= 0; --i) {
+        if (objectArray[i]->IsDead ()) {
+            objectArray.erase (objectArray.begin () + i);
+        }
+    }
 }
 
 void State::Render () {
     bg.Render (0, 0);
+    for (unsigned int i = 0; i < objectArray.size (); ++i) {
+        objectArray[i]->Render ();
+    }
+}
+
+void State::Input() {
+    SDL_Event event;
+    int mouseX, mouseY;
+
+    SDL_GetMouseState (&mouseX, &mouseY);
+    while (SDL_PollEvent (&event)) {
+        if (event.type == SDL_QUIT) {
+            quitRequested = true;
+        }
+        if (event.type == SDL_MOUSEBUTTONDOWN) {
+            for (int i = objectArray.size () - 1; i >= 0; --i) {
+                Face* face = (Face*) objectArray[i].get ();
+                if(face->box.IsInside ((float)mouseX, (float)mouseY)) {
+                    face->Damage (rand () % 10 + 10);
+                    break;
+                }
+            }
+        }
+        if (event.type == SDL_KEYDOWN) {
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                quitRequested = true;
+            } else {
+                AddObject ((float)mouseX, (float)mouseY);
+            }
+        }
+    }
+}
+
+void State::AddObject (float mouseX, float mouseY) {
+    Vec2 pos(mouseX, mouseY);
+    Vec2 dir = Vec2::FromPolar (200, 2*3.141592*(float)rand ()/RAND_MAX);
+    pos = pos + dir;
+    objectArray.emplace_back (new Face (pos.x, pos.y));
 }
