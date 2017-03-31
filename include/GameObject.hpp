4f364d954c1ea895240a6ba7d49847874ceb59b3
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-03-31 03:10:46 -0300

    Cria classe GameObject

---
+++ include/GameObject.hpp
@@ -0,0 +1,17 @@
+#ifndef _GAMEOBJECT_HPP_
+#define _GAMEOBJECT_HPP_
+
+class GameObject;
+
+#include "Rect.hpp"
+
+class GameObject {
+  public:
+    virtual ~GameObject ();
+    virtual void Update (float dt) = 0;
+    virtual void Render () = 0;
+    virtual bool IsDead () = 0;
+    Rect box;
+};
+
+#endif //_GAMEOBJECT_HPP_
