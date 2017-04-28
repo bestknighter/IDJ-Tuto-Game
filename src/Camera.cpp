912753a571af122a058ce0556dda6ac7f06a0305
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-04-27 23:37:53 -0300

    Muda o movimento da camera para ser so com teclas

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -19,19 +19,19 @@ void Camera::Update (float dt) {
         pos = focus->box.GetCentro ();
     } else { // Se nao, calcule a nova velocidade de movimento da camera baseado no teclado
         Vec2 newSpeed; // TODO Refatorar quando Vec2 estiver mais completo
-        if (IMinstance.IsKeyDown (UP_ARROW_KEY) || IMinstance.IsKeyDown (W_KEY)) { // Mover camera para cima com UP ou W
+        if (IMinstance.IsKeyDown (UP_ARROW_KEY)) { // Mover camera para cima com UP
             newSpeed = {0, -BASE_CAM_SPEED};
             speed = speed + newSpeed;
         }
-        if (IMinstance.IsKeyDown (DOWN_ARROW_KEY) || IMinstance.IsKeyDown (S_KEY)) { // Mover camera para baixo com DOWN ou S
+        if (IMinstance.IsKeyDown (DOWN_ARROW_KEY)) { // Mover camera para baixo com DOWN
             newSpeed = {0, BASE_CAM_SPEED};
             speed = speed + newSpeed;
         }
-        if (IMinstance.IsKeyDown (RIGHT_ARROW_KEY) || IMinstance.IsKeyDown (D_KEY)) { // Mover camera para direita com RIGHT ou D
+        if (IMinstance.IsKeyDown (RIGHT_ARROW_KEY)) { // Mover camera para direita com RIGHT
             newSpeed = {BASE_CAM_SPEED, 0};
             speed = speed + newSpeed;
         }
-        if (IMinstance.IsKeyDown (LEFT_ARROW_KEY) || IMinstance.IsKeyDown (A_KEY)) { // Mover camera para esquerda com LEFT ou A
+        if (IMinstance.IsKeyDown (LEFT_ARROW_KEY)) { // Mover camera para esquerda com LEFT
             newSpeed = {-BASE_CAM_SPEED, 0};
             speed = speed + newSpeed;
         }
